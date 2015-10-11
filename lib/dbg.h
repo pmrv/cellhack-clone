a684f205ebad384ffe95d26df5a91e696efd19a1
Marvin Poul <ponder@creshal.de>
2015-10-11 14:05:37 +0200

    use consistent debug flags

--- lib/dbg.h
+++ lib/dbg.h
@@ -5,7 +5,7 @@
 #include "errno.h"
 #include "string.h"
 
-#ifdef NDEBUG
+#ifndef DEBUG
 #define debug(M, ...)
 #else
 #define debug(M, ...) fprintf (stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
